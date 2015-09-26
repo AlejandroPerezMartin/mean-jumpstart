61f27363a0f986f16fc54a944f7da67912c91c5e
Alejandro <aleperez92@gmail.com>
2015-09-26 13:44:01 +0100

    Added local authentication

--- module-3/routes/api.js
+++ module-3/routes/api.js
@@ -1,13 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function (req, res, next) {
+    if (req.method === 'GET') {
+        return next();
+    }
+
+    if (!req.isAuthenticated()) {
+        // user not authenticated, redirect to login page
+        return res.redirect('/#login')
+    }
+});
+
 router.route('/posts')
     // return all posts
-    .get(function(req, res) {
-        res.send({message: 'Todo: return all posts'});
+    .get(function (req, res) {
+        res.send({
+            message: 'TODO: Return all posts'
+        });
     })
-    .post(function(req, res){
-        res.send({message: 'Todo: Create new post'});
+    .post(function (req, res) {
+        res.send({
+            message: 'TODO: Create new post'
+        });
+    });
+
+router.route('/posts/:id')
+    .get(function (req, res) {
+        res.send({
+            message: 'TODO: Return post with ID ' + req.params.id
+        });
+    })
+    .put(function (req, res) {
+        res.send({
+            message: 'TODO: Modify post with ID ' + req.params.id
+        });
+    })
+    .delete(function (req, res) {
+        res.send({
+            message: 'TODO: Delete post with ID ' + req.params.id
+        });
     });
 
 module.exports = router;
